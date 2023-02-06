9a5d168cd1d0fd413d40aa4b2a6c0eb790c53032
Mykhailo Tsynkevych <tsynkevych@gmx.de>
2023-02-06 15:07:19 +0100

    styles

--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -1,54 +1,55 @@
 import s from "./TransactionForm.module.css";
 
 const TransactionForm = () => {
-    return (
-        <form
-          name="transactionForm"
-          autoComplete="off"
-          noValidate
-          className={s.transactionForm}
-        >
-          <label>
-            Transaction
-            <select name="transaction-art">
-              <option value="Income">Income</option>
-              <option value="expense" defaultValue>
-                Expense
-              </option>
-            </select>
-          </label>
-          <label>
-            Date
-            <input type="date" name="date" />
-          </label>
-  
-          <label>
-            Time
-            <input type="time" name="time" />
-          </label>
+  return (
+    <form
+      name="transactionForm"
+      autoComplete="off"
+      noValidate
+      className={s.transactionForm}
+    >
+      <label>
+        Transaction
+        <select name="transaction-art">
+          <option value="Income">Income</option>
+          <option value="expense" defaultValue>
+            Expense
+          </option>
+        </select>
+      </label>
+      <div className={s.timeWrapper}>
+        <label>
+          Date and Time
+          <input type="date" name="date" />
+        </label>
 
-          <label>
-            Category
-            <select name="category">
-              <option value="Flat">Flat</option>
-              <option value="Food" defaultValue>
-              Food
-              </option>
-              <option value="Auto">Auto</option>
-            </select>
-          </label>
+        <label>
+          <input type="time" name="time" />
+        </label>
+        
+      </div>
 
-          <label>
-            Sum
-            <input type="number" name="sum" step="1" min="0" />
-          </label>
+      <label>
+        Category
+        <select name="category">
+          <option value="Flat">Flat</option>
+          <option value="Food" defaultValue>
+            Food
+          </option>
+          <option value="Auto">Auto</option>
+        </select>
+      </label>
 
-          <label>
-            Comment
-            <textarea name="comment" rows="5" placeholder="Comment"></textarea>
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-    );
-  };
-  export default TransactionForm;
+      <label className={s.greybgc}>
+        Sum
+        <input type="number" name="sum" step="1" min="0" defaultValue="0"/>
+      </label>
+
+      <label>
+        <textarea name="comment" rows="1" placeholder="Comment"></textarea>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default TransactionForm;
