1f81d796572ae4ae2d2c73aaf85763c80d654fe7
Mykhailo Tsynkevych <tsynkevych@gmx.de>
2023-02-18 14:25:22 +0100

    fix bugs3 with online preview on iphone

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,22 +1,25 @@
 import styled from "styled-components";
 
-const StyledMainTitle = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-`;
-
 const StyledHeaderWrapper = styled.header`
   display: flex;
   align-items: center;
+  padding: 5px 8px;
   background-color: rgb(225, 132, 52);
   box-shadow: 0px 2px 2px rgb(0 0 0 / 10%);
 `;
 
+const StyledMainTitle = styled.h1`
+font-size: 24px;
+  text-align: center;
+`;
+
+
 const StyledMenuBurger = styled.button`
 background-color: inherit;
-width: 36px;
-margin: 5px;
+width: 26px;
 transition: transform 0.5s linear;
+padding: 0px;
+margin-right: 10px;
 
 :hover {
   cursor: pointer;
