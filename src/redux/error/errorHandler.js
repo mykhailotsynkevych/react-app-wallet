c9ff1a4f70efd9d8bc97095efd4450850a3f7aad
Mykhailo Tsynkevych <tsynkevych@gmx.de>
2024-01-16 10:54:27 +0100

    test reBuild

--- src/redux/error/errorHandler.js
+++ src/redux/error/errorHandler.js
@@ -3,7 +3,6 @@ import { refreshToken } from "../auth/authOperations";
 export const errorHandler =
   ({ error, cb }) =>
   (dispatch) => {
-    // console.log(object);
     if (error.request.status === 400 || error.request.status === 401) {
       dispatch(refreshToken(cb));
     }
